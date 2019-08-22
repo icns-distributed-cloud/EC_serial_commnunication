4136c7dc285bc5b5ce5714e466c54a24eaef3468
Yunhwan Kim <pup0929@gmail.com>
2019-08-22 16:26:43 +0900

    Add LICENSE information to source code
    - Add LICENSE information
    - Add source file information

--- serial_input.py
+++ serial_input.py
@@ -1,3 +1,42 @@
+# This file is a part of EC_serial_communication
+# BSD 3-Clause License
+#
+# Copyright (c) 2019, Intelligent-distributed Cloud and Security Laboratory (ICNS Lab.)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# title           : serial_input.py
+# description     : Python, edge cloud serial communication
+# author          : Yunhwan Kim.
+# date            : 20190822
+# version         : 0.3
+# notes           : This EC_serial_communication is an implementation of edge cloud for communicating
+# 				 self-driving cart and edge cloud.
+
 import threading
 import time
 import numpy as np
